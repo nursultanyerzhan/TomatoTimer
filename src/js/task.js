9048e033af27fd33b4071d6223213bf6f74f1374
Нурсултан <nursultanerzhan9301@gmail.com>
2022-06-12 21:32:48 +0600

    baseClass

---
+++ src/js/task.js
@@ -0,0 +1,24 @@
+export class Task {
+    #id;
+    #name;
+    #counter;
+    constructor(name, counter = 0) {
+        this.#id = Math.round(Math.random() * 10000000);
+        this.#name = name;
+        this.#counter = counter;
+    }
+
+    getId() { return this.#id; }
+
+    setName(newName) {
+        this.#name = newName;
+    }
+
+    getName() { return this.#name; }
+
+    incrementCount() {
+        this.#counter++;
+    }
+
+    getCounter() { return this.#counter; }
+}
